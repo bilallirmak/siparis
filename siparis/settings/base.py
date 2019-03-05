e770ca4986e7a04d7b276a31aa42623f59fcdc7f
Bilal Irmak <bilal.irmak00@gmail.com>
2019-03-06 00:12:59 +0300

    Hosting settings update

--- siparis/settings/base.py
+++ siparis/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'xv2+@0&)8$$51lpyj=s!7-5bfg)1+c03h2%r3hi3j6jznf3-zd'
 # DEBUG = True
 
 # ALLOWED_HOSTS = ['inciregemutfak.site', 'www.inciregemutfak.site', '142.93.237.83']
-ALLOWED_HOSTS = ['inciregemutfak.site', 'www.inciregemutfak.site', '142.93.237.83']
+ALLOWED_HOSTS = []
 
 # Application definition
 

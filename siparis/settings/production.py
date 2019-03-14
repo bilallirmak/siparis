4e303bb8446a6a185bd2bfb543f7a0dbc394db27
= <=>
2019-03-14 18:58:08 +0300

    test

--- siparis/settings/production.py
+++ siparis/settings/production.py
@@ -10,7 +10,7 @@ DATABASES = {
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media", "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # """
